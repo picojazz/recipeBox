f574929da4b63d32c0cbc9fb81a0fa5bee8db2f2
picojazz <picojazz@gmail.com>
2019-01-22 13:51:00 +0000

    templating shopping section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('tigedege', 5),
+    new Ingredient('potatoes',6)
+  ]
 
   constructor() { }
 
